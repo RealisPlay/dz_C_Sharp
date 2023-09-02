470bc6ed99b2385ea3438130f1327ab601451027
Realis <maxbytski@mail.ru>
2023-09-02 14:38:36 +0700

    дз сделал

--- Program.cs
+++ Program.cs
@@ -23,26 +23,53 @@ else
 44 5 78 -> 78
 22 3 9 -> 22*/
 
-Console.WriteLine ("Введите число №1: ");
+/*Console.WriteLine ("Введите число №1: ");
 int Num1 = Convert.ToInt32(Console.ReadLine());;
 Console.WriteLine ("Введите число №2: ");
 int Num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите число №3: ");
 int Num3 = Convert.ToInt32(Console.ReadLine());
-int Max;
-if (Num1 > Num2)
+int Max = Num1;
+if (Num2 > Max)
  {
-    Console.WriteLine(Max = Num1);
+    Max = Num2;
+ }
+ if (Num3 > Max)
+ {
+    Max = Num3;
  }
  else
  {
-    Console.WriteLine(Max = Num2);
+    Max = Max;
  }
-if (Max > Num3)
+Console.WriteLine (Max);*/
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+/*Console.WriteLine ("Введите целое число");
+int Num = Convert.ToInt32(Console.ReadLine());
+if ((Num % 2) == 0)
 {
-    Console.WriteLine(Max = Num3);
+    Console.WriteLine("четное");
 }
 else
 {
-    Console.WriteLine(Max);
-}
+    Console.WriteLine("нечетное");
+}*/
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+/*Console.WriteLine ("Введите целое число");
+int Num = Convert.ToInt32(Console.ReadLine());
+int NumFirst = 2;
+while(NumFirst <= Num)
+{
+    Console.Write(NumFirst);
+    Console.Write(",");
+    NumFirst = NumFirst + 2;
+}*/
