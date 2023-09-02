680bc266ce580c205d96773a8b4d8a54405cb5d2
Realis <maxbytski@mail.ru>
2023-09-02 13:15:15 +0700

    save1

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
 
-Console.WriteLine ("Введите число №1: ");
+/*Console.WriteLine ("Введите число №1: ");
 int Num1 = Convert.ToInt32(Console.ReadLine());;
 Console.WriteLine ("Введите число №2: ");
 int Num2 = Convert.ToInt32(Console.ReadLine());
@@ -16,4 +16,33 @@ else
 {
     Console.WriteLine(Num2);Console.WriteLine("большее");
     Console.WriteLine(Num1);Console.WriteLine("меньшее");
+}*/
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.WriteLine ("Введите число №1: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());;
+Console.WriteLine ("Введите число №2: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число №3: ");
+int Num3 = Convert.ToInt32(Console.ReadLine());
+int Max;
+if (Num1 > Num2)
+ {
+    Console.WriteLine(Max = Num1);
+ }
+ else
+ {
+    Console.WriteLine(Max = Num2);
+ }
+if (Max > Num3)
+{
+    Console.WriteLine(Max = Num3);
+}
+else
+{
+    Console.WriteLine(Max);
 }
